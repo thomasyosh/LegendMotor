28b83f27829db573d21b1b0312bde45b63ebe571
Thomas LI <220297115@stu.vtc.edu.hk>
2024-06-11 09:27:15 +0800

    third

--- LegendMotor.WinForm/LoginForm.Designer.cs
+++ LegendMotor.WinForm/LoginForm.Designer.cs
@@ -28,98 +28,118 @@ partial class LoginForm
     /// </summary>
     private void InitializeComponent()
     {
-        this.txt_username = new System.Windows.Forms.TextBox();
-        this.txt_password = new System.Windows.Forms.TextBox();
-        this.lbl_username = new System.Windows.Forms.Label();
-        this.lbl_password = new System.Windows.Forms.Label();
-        this.btn_login = new System.Windows.Forms.Button();
-        this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
-        this.pictureBox1 = new System.Windows.Forms.PictureBox();
-        ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-        this.SuspendLayout();
+        txt_username = new TextBox();
+        txt_password = new TextBox();
+        lbl_username = new Label();
+        lbl_password = new Label();
+        btn_login = new Button();
+        backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+        pictureBox1 = new PictureBox();
+        label1 = new Label();
+        ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+        SuspendLayout();
         // 
         // txt_username
         // 
-        this.txt_username.Font = new System.Drawing.Font("PMingLiU", 16F);
-        this.txt_username.Location = new System.Drawing.Point(214, 264);
-        this.txt_username.Name = "txt_username";
-        this.txt_username.Size = new System.Drawing.Size(291, 33);
-        this.txt_username.TabIndex = 1;
-        this.txt_username.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.CheckEnterKeyPress);
+        txt_username.Font = new Font("新細明體", 16F);
+        txt_username.Location = new Point(250, 330);
+        txt_username.Margin = new Padding(4, 4, 4, 4);
+        txt_username.Name = "txt_username";
+        txt_username.Size = new Size(339, 33);
+        txt_username.TabIndex = 1;
+        txt_username.KeyPress += CheckEnterKeyPress;
         // 
         // txt_password
         // 
-        this.txt_password.Font = new System.Drawing.Font("PMingLiU", 16F);
-        this.txt_password.Location = new System.Drawing.Point(214, 316);
-        this.txt_password.Name = "txt_password";
-        this.txt_password.Size = new System.Drawing.Size(291, 33);
-        this.txt_password.TabIndex = 2;
-        this.txt_password.UseSystemPasswordChar = true;
-        this.txt_password.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.CheckEnterKeyPress);
+        txt_password.Font = new Font("新細明體", 16F);
+        txt_password.Location = new Point(250, 395);
+        txt_password.Margin = new Padding(4, 4, 4, 4);
+        txt_password.Name = "txt_password";
+        txt_password.Size = new Size(339, 33);
+        txt_password.TabIndex = 2;
+        txt_password.UseSystemPasswordChar = true;
+        txt_password.KeyPress += CheckEnterKeyPress;
         // 
         // lbl_username
         // 
-        this.lbl_username.AutoSize = true;
-        this.lbl_username.Font = new System.Drawing.Font("PMingLiU", 16F);
-        this.lbl_username.Location = new System.Drawing.Point(109, 267);
-        this.lbl_username.Name = "lbl_username";
-        this.lbl_username.Size = new System.Drawing.Size(99, 22);
-        this.lbl_username.TabIndex = 3;
-        this.lbl_username.Text = "Username:";
+        lbl_username.AutoSize = true;
+        lbl_username.Font = new Font("新細明體", 16F);
+        lbl_username.Location = new Point(127, 334);
+        lbl_username.Margin = new Padding(4, 0, 4, 0);
+        lbl_username.Name = "lbl_username";
+        lbl_username.Size = new Size(99, 22);
+        lbl_username.TabIndex = 3;
+        lbl_username.Text = "Username:";
         // 
         // lbl_password
         // 
-        this.lbl_password.AutoSize = true;
-        this.lbl_password.Font = new System.Drawing.Font("PMingLiU", 16F);
-        this.lbl_password.Location = new System.Drawing.Point(114, 319);
-        this.lbl_password.Name = "lbl_password";
-        this.lbl_password.Size = new System.Drawing.Size(94, 22);
-        this.lbl_password.TabIndex = 4;
-        this.lbl_password.Text = "Password:";
+        lbl_password.AutoSize = true;
+        lbl_password.Font = new Font("新細明體", 16F);
+        lbl_password.Location = new Point(133, 399);
+        lbl_password.Margin = new Padding(4, 0, 4, 0);
+        lbl_password.Name = "lbl_password";
+        lbl_password.Size = new Size(94, 22);
+        lbl_password.TabIndex = 4;
+        lbl_password.Text = "Password:";
         // 
         // btn_login
         // 
-        this.btn_login.Dock = System.Windows.Forms.DockStyle.Bottom;
-        this.btn_login.Font = new System.Drawing.Font("PMingLiU", 16F);
-        this.btn_login.Location = new System.Drawing.Point(0, 369);
-        this.btn_login.Name = "btn_login";
-        this.btn_login.Size = new System.Drawing.Size(628, 38);
-        this.btn_login.TabIndex = 5;
-        this.btn_login.Text = "Login";
-        this.btn_login.UseVisualStyleBackColor = true;
-        this.btn_login.Click += new System.EventHandler(this.btn_login_Click);
+        btn_login.Dock = DockStyle.Bottom;
+        btn_login.Font = new Font("新細明體", 16F);
+        btn_login.Location = new Point(0, 461);
+        btn_login.Margin = new Padding(4, 4, 4, 4);
+        btn_login.Name = "btn_login";
+        btn_login.Size = new Size(733, 48);
+        btn_login.TabIndex = 5;
+        btn_login.Text = "Login";
+        btn_login.UseVisualStyleBackColor = true;
+        btn_login.Click += btn_login_Click;
         // 
         // pictureBox1
         // 
-        this.pictureBox1.Image = global::LegendMotor.WinForm.Properties.Resources.logo;
-        this.pictureBox1.Location = new System.Drawing.Point(12, 12);
-        this.pictureBox1.Name = "pictureBox1";
-        this.pictureBox1.Size = new System.Drawing.Size(604, 246);
-        this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
-        this.pictureBox1.TabIndex = 6;
-        this.pictureBox1.TabStop = false;
+        pictureBox1.Image = Properties.Resources.logo;
+        pictureBox1.Location = new Point(14, 15);
+        pictureBox1.Margin = new Padding(4, 4, 4, 4);
+        pictureBox1.Name = "pictureBox1";
+        pictureBox1.Size = new Size(705, 308);
+        pictureBox1.SizeMode = PictureBoxSizeMode.CenterImage;
+        pictureBox1.TabIndex = 6;
+        pictureBox1.TabStop = false;
+        // 
+        // label1
+        // 
+        label1.AutoSize = true;
+        label1.BackColor = Color.White;
+        label1.Font = new Font("標楷體", 26.25F, FontStyle.Regular, GraphicsUnit.Point, 136);
+        label1.Location = new Point(188, 246);
+        label1.Margin = new Padding(4, 0, 4, 0);
+        label1.Name = "label1";
+        label1.Size = new Size(339, 35);
+        label1.TabIndex = 7;
+        label1.Text = "學海無涯　回頭是岸";
         // 
         // LoginForm
         // 
-        this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
-        this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-        this.AutoSize = true;
-        this.ClientSize = new System.Drawing.Size(628, 407);
-        this.Controls.Add(this.pictureBox1);
-        this.Controls.Add(this.btn_login);
-        this.Controls.Add(this.lbl_password);
-        this.Controls.Add(this.lbl_username);
-        this.Controls.Add(this.txt_password);
-        this.Controls.Add(this.txt_username);
-        this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
-        this.MaximizeBox = false;
-        this.MinimizeBox = false;
-        this.Name = "LoginForm";
-        this.Text = "Login";
-        ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-        this.ResumeLayout(false);
-        this.PerformLayout();
-
+        AutoScaleDimensions = new SizeF(7F, 15F);
+        AutoScaleMode = AutoScaleMode.Font;
+        AutoSize = true;
+        ClientSize = new Size(733, 509);
+        Controls.Add(label1);
+        Controls.Add(pictureBox1);
+        Controls.Add(btn_login);
+        Controls.Add(lbl_password);
+        Controls.Add(lbl_username);
+        Controls.Add(txt_password);
+        Controls.Add(txt_username);
+        FormBorderStyle = FormBorderStyle.FixedSingle;
+        Margin = new Padding(4, 4, 4, 4);
+        MaximizeBox = false;
+        MinimizeBox = false;
+        Name = "LoginForm";
+        Text = "Login";
+        ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+        ResumeLayout(false);
+        PerformLayout();
     }
 
     #endregion
@@ -130,5 +150,6 @@ partial class LoginForm
     private System.Windows.Forms.Button btn_login;
     private System.ComponentModel.BackgroundWorker backgroundWorker1;
     private System.Windows.Forms.PictureBox pictureBox1;
+    private Label label1;
 }
 
--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 .Net SDK 8.0 or above is required for this project.
 https://dotnet.microsoft.com/en-us/download/dotnet/8.0
+
+![Demo](LegendMotor.WinForm/Resources/logo.jpg)
+
+Username:
+-	admin
+-	storemen
+-	sales
+-	salemanager
+-	areamanager
+-	purchase
+
+Password:
+abcd1234
